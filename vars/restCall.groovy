bed79296db0158a91fdd509aa83cfe1495cf497a
kdunikowski <ko.dunikowski@gmail.com>
2023-06-22 15:08:17 +0200

    Update restCall.groovy

--- vars/restCall.groovy
+++ vars/restCall.groovy
@@ -2,14 +2,9 @@
 def call(String message) {
     def postmanPost = new URL('https://postman-echo.com/post')
     def postConnection = postmanPost.openConnection()
+    postConnection.requestMethod = 'POST'
     def form = "param1=This is request parameter."
     postConnection.doOutput = true
-    def text
-    postConnection.with {
-        outputStream.withWriter { outputStreamWriter ->
-            outputStreamWriter << form
-        }
-        text = content.text
-    }
     echo 'INFO: 'postConnection.responseCode
+    echo 'INFO': 'postConnection
 }
